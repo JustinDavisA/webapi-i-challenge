1a5bda5cdcf74141b39afb94170a3dd5decf9370
justindavisa <justindavisa@gmail.com>
2019-03-18 17:49:37 -0400

    Setup CRUD methods

--- index.js
+++ index.js
@@ -5,3 +5,21 @@ const express = require('express');
 const db = require('./data/db.js');
 
 const server = express();
+
+// CRUD
+
+// Create
+server.post('/', (req, res) => {
+});
+
+// Read
+server.get('/', (req, res) => {
+});
+
+// Update
+server.put('/', (req, res) => {
+});
+
+// Delete
+server.delete('/', (req, res) => {
+});
